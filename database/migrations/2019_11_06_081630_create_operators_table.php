a1d883963cc37ff5a0b3f254bf0301e51f9a094b
Bagenda Adolf <bagenda.adolf@gmail.com>
2019-11-07 01:16:51 +0300

    feature operators

---
+++ database/migrations/2019_11_06_081630_create_operators_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOperatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operators', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('address');
+            $table->string('code',5)->nullable();
+            $table->string('logo')->nullable();
+            $table->string('email')->nullable();
+            $table->string('contact_person_name');
+            $table->string('phone_number');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+}
