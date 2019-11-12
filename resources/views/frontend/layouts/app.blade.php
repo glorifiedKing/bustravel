f2ea677deb42ffe061859dbb402007ba628fc6bb
Raymond <rayim25@gmil.com>
2019-11-12 11:23:15 -0800

    frontend base

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,17 +4,37 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title', config('adminlte.title', 'PalmKash'))</title>
+  <meta name="msapplication-TileColor" content="#0F1624">
+  <meta name="theme-color" content="#0F1624">
+  <title>@yield('title', config('adminlte.title', 'PalmKash Bus Ticketing System'))</title>
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('vendor/glorifiedking/fav/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('vendor/glorifiedking/fav/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('vendor/glorifiedking/fav/favicon-16x16.png') }}">
+  <link rel="shortcut icon" href="{{ asset('fav/favicon.png') }}">
   <link href="{{ asset('vendor/glorifiedking/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-  <link href="{{ asset('vendor/glorifiedking/css/app.css') }}" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('vendor/glorifiedking/css/base.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('vendor/glorifiedking/css/styles.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('vendor/glorifiedking/css/queries.css') }}">
+  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+
+  
 </head>
 <body>
-  <div class="container">
-    @yield('content')
-  </div>
+    <div class="header-title">
+        <h1>@yield('page-heading',config('adminlte.title', ''))</h1>
+    </div>
+    @include('bustravel::frontend.partials.header')
+    <main>
+      <div class="container-fluid">
+        @yield('content')
+      </div>
+    </main>
+    <svg id="all-svg" width="0" height="0"></svg>
   <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>	
-  <script src="{{ asset('vendor/glorifiedking/js/app.js') }}" type="text/js"></script>
+  <script src="{{ asset('vendor/glorifiedking/js/bootstrap.min.js') }}"></script>  
+  <script type="text/javascript" src="{{ asset('vendor/glorifiedking/js/modnz.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('vendor/glorifiedking/js/aos.js') }}"></script>  
+  <script type="text/javascript" src="{{ asset('vendor/glorifiedking/js/scripts.js') }}"></script>
 </body>
 </html>
 
---
+++ resources/views/frontend/login.blade.php
@@ -0,0 +1,42 @@
+@extends('bustravel::frontend.layouts.app')
+@section('title', 'PalmKash Bus Ticketing Homepage')
+@section('page-heading','Bus Ticketing System')    
+
+
+            @section('content')
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1 class="h3 mb-3 font-weight-normal">Account</h1>
+                        <ul class="form-header-custom">
+                            <li class="active">Login</li>
+                            <li class="divider-custom"></li>
+                            <li><a href="{{route('register')}}">Sign Up</a></li>
+                        </ul>
+                        <form class="sign-in-up" method="post" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputEmail4">Email</label>
+                                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="inputEmail4" placeholder="jane@doe.com">
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group col-md-6 pos-rel">
+                                    <label for="inputPassword4">Password</label>
+                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="inputPassword4" placeholder="****">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                    <a href="" class="forgot-pass">Forgot Password</a>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary cust-btn-link">Login</button>
+                        </form>
+                    </div>
+                </div>
+                @endsection
