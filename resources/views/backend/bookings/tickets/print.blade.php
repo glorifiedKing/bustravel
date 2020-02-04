0677961a5f1643491bf63c9106fb9d6c302e0dd1
Raymond <rayim25@gmil.com>
2020-02-04 02:20:35 -0800

    ticket printing

---
+++ resources/views/backend/bookings/tickets/print.blade.php
@@ -0,0 +1,20 @@
+<style>
+    p {
+        font-size: 2pt
+    }
+</style>
+<!-- logo -->
+<img src="" >
+<h4>Bus Travel Ticket</h4>
+<!-- route details -->
+<p>Ticket no:{{$booking->ticket_number}} </p>
+<p>Date of Travel: {{$booking->date_of_travel}}</p>
+<p>Bus Reg No: {{$bus_reg_no ?? 'rw 521s'}}</p>
+<p>From: {{$departure_station ?? 'NYABUGABO'}}</p>
+<p>At: {{$departure_time ?? '10:00'}}</p>
+<p>To: {{$destination_station ?? 'HUYE'}}</p>
+<p>At: {{$arrival_time ?? '12:00'}}</p>
+
+<!-- powered by palm cash -->
+<p>Powered by Palm Kash</p>
+<p>www.palmkash.com</p>
