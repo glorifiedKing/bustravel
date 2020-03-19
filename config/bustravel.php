cadb72c67b0a80ac87560dc6b4048724c7a7cced
Raymond Byaruhanga <11267178+glorifiedKing@users.noreply.github.com>
2020-03-19 18:11:08 +0300

    Update bustravel.php

--- config/bustravel.php
+++ config/bustravel.php
@@ -3,4 +3,11 @@
 return [
     'path'       => 'transit',
     'user_model' => App\User::class,
+    'payment_gateways' => [
+        'mtn_rw' => [
+            'username' => 'palm_kash',
+            'password' => '12345',
+            'url' => 'https://mtn.co.rw:8080',
+         ],
+    ],
 ];
