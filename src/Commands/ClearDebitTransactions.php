3dcc9cbceb498854b62e4a82e8202a73058a5a3c
Raymond Byaruhanga <11267178+glorifiedKing@users.noreply.github.com>
2020-04-13 16:41:34 +0300

    Update ClearDebitTransactions.php

--- src/Commands/ClearDebitTransactions.php
+++ src/Commands/ClearDebitTransactions.php
@@ -84,7 +84,7 @@ class ClearDebitTransactions extends Command
                     $response_body = json_decode($checkstatus->getBody(),true);
                     // log request
                     $status_variables = var_export($response_body,true);
-                    $status_log = date('Y-m-d H:i:s')." WITH:".$status_variables."";
+                    $status_log = date('Y-m-d H:i:s')." transaction_id: ".$transaction->id." WITH:".$status_variables."";
                     //log the request 
                     \Storage::disk('local')->append('payment_checkstatus_log.txt',$status_log);
                     $new_transaction_status = $response_body['transaction_status'];
@@ -223,7 +223,7 @@ class ClearDebitTransactions extends Command
                                 $response_body = json_decode($checkstatus->getBody(),true);
                                 // log request
                                 $status_variables = var_export($response_body,true);
-                                $status_log = date('Y-m-d H:i:s')." WITH:".$status_variables."";
+                                $status_log = date('Y-m-d H:i:s')." transaction_id: 1".$transaction->id." WITH:".$status_variables."";
                                 //save the request 
                                 \Storage::disk('local')->append('payment_credit_request_log.txt',$status_log);
                                 $new_transaction_status = $response_body['transaction_status'];
@@ -240,7 +240,7 @@ class ClearDebitTransactions extends Command
                             }
                             catch(\Exception $e)
                             {
-                                $error_log = date('Y-m-d H:i:s')." error:".$e->getMessage()."";
+                                $error_log = date('Y-m-d H:i:s')." transaction_id: 1".$transaction->id." error:".$e->getMessage()."";
                                 \Storage::disk('local')->append('payment_credit_request_log.txt',$error_log);
 
                             }
