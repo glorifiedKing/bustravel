66720ac39b9020937aa93aaf29303b042be6bbc5
Raymond Byaruhanga <rayim25@gmail.com>
2020-04-25 12:32:49 +0300

    ussd payment

---
+++ database/migrations/2020_04_25_091630_add_payment_source_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPaymentSourceBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            if (!Schema::hasColumn('bookings', 'payment_source')) {
+                $table->string('payment_source')->default('web');
+                $table->unsignedBigInteger('payment_transaction_id')->nullable();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn('payment_source');
+            $table->dropColumn('payment_transaction_id');
+        });
+    }
+}
