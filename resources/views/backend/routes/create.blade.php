f8dcdc029dad4047965a65a8846445d7300d4dba
mtvbrianking <mtvbrianking@gmail.com>
2020-05-21 23:29:08 +0300

    Build routes from stations.

--- resources/views/backend/routes/create.blade.php
+++ resources/views/backend/routes/create.blade.php
@@ -22,142 +22,55 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-            <h5 class="card-title">Add Route </h5>
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">Add Route</h5>
+                </div>
+                <div class="card-body">
+                    <form id="add-stations-form" method="GET">
+                        <div class="row">
+                            <div class="col-md-3 mb-4">
+                                <select class="form-control" name="station" required>
+                                    <option value="" selected>Choose station...</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2 mb-4">
+                                <input type="time" class="form-control" name="time" value="" placeholder="00:00:00" required>
+                            </div>
+                            <div class="col-md-2 mb-4">
+                                <input type="submit" class="btn btn-block btn-primary" id="add" value="Add">
+                            </div>
+                            <div class="col-md-2 mb-4">
+                                <input type="button" class="btn btn-block btn-danger" id="remove" value="Remove">
+                            </div>
+                            <div class="col-md-2 mb-4">
+                                <input type="button" class="btn btn-block btn-success" id="generate" value="Generate">
+                            </div>
+                        </div>
+                    </form>
+                    <div class="row" id="stations"></div>
+                    <form method="POST" action="">
+                        <div class="responsive" id="routes">
+                            <table class="table" id="routes-tbl">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">From</th>
+                                        <th scope="col">To</th>
+                                        <th scope="col">In</th>
+                                        <th scope="col">Out</th>
+                                        <th scope="col">Price</th>
+                                        <th scope="col">Order</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-            <div class="row">
-              <div class="col-md-12">
-              <form role="form" action="{{route('bustravel.routes.store')}}" method="POST" >
-              {{csrf_field() }}
-
-              <div class="box-body">
-                  <div class="row">
-                    <div class="form-group col-md-3 ">
-                      <label>Start Station</label>
-                      <select class="form-control select2 {{ $errors->has('start_station') ? ' is-invalid' : '' }}" name="start_station"  placeholder="Select Operator">
-                        <option value="">Select Station</option>
-                        @foreach($stations as $station)
-                            <option value="{{$station->id}}" @php echo old('start_station') == $station->id ? 'selected' :  "" @endphp>{{$station->name}} - {{$station->code}}</option>
-                        @endforeach
-                      </select>
-                      @if ($errors->has('start_station'))
-                          <span class="invalid-feedback">
-                              <strong>{{ $errors->first('start_station') }}</strong>
-                          </span>
-                      @endif
-                    </div>
-                    <div class="form-group col-md-3 ">
-                      <label>End Station</label>
-                      <select class="form-control select2 {{ $errors->has('end_station') ? ' is-invalid' : '' }}" name="end_station"  placeholder="Select Operator">
-                        <option value="">Select Station</option>
-                        @foreach($stations as $station)
-                            <option value="{{$station->id}}" @php echo old('end_station') == $station->id ? 'selected' :  "" @endphp>{{$station->name}} - {{$station->code}}</option>
-                        @endforeach
-                      </select>
-                      @if ($errors->has('end_station'))
-                          <span class="invalid-feedback">
-                              <strong>{{ $errors->first('end_station') }}</strong>
-                          </span>
-                      @endif
-                    </div>
-                    <div class="form-group col-md-3 ">
-                      <label>Price</label>
-                      <input type="text"  name="price" value="{{old('price')}}" class="form-control {{ $errors->has('price') ? ' is-invalid' : '' }}" id="exampleInputEmail1" placeholder="Enter Price" >
-                      @if ($errors->has('price'))
-                          <span class="invalid-feedback">
-                              <strong>{{ $errors->first('price') }}</strong>
-                          </span>
-                      @endif
-                    </div>
-                    <div class="form-group col-md-3 ">
-                      <label>Return Price</label>
-                      <input type="text"  name="return_price" value="{{old('return_price')}}" class="form-control {{ $errors->has('price') ? ' is-invalid' : '' }}" id="exampleInputEmail1" placeholder="Enter Return Price" >
-                      @if ($errors->has('return_price'))
-                          <span class="invalid-feedback">
-                              <strong>{{ $errors->first('return_price') }}</strong>
-                          </span>
-                      @endif
-                    </div>
-                    <div class="form-group col-md-12"><label>Stopovers Routes</label></div>
-                    <div class="form-group col-md-12">
-                    <div class="row">
-                      <div class="form-group col-md-3">
-                        <label>Start</label>
-                        <select id="item-selector" class="form-control select2" name="select_startitem_id">
-                           @foreach($stations as $station_start)
-                               <option data-startitemid="{{$station_start->id}}" data-startitemname="{{$station_start->name}}"  value="{{$station_start->id}}">{{$station_start->name}} - {{$station_start->code}}</option>
-                           @endforeach
-                        </select>
-                      </div>
-                     <div class="form-group col-md-3">
-                       <label>End</label>
-                       <select id="item-selector2" class="form-control select2" name="select_enditem_id">
-                           @foreach($stations as $station_end)
-                               <option data-enditemid="{{$station_end->id}}" data-enditemname="{{$station_end->name}}"  value="{{$station_end->id}}">{{$station_end->name}} - {{$station_end->code}}</option>
-                           @endforeach
-                       </select>
-                     </div>
-                    <div class="form-group col-md-2"><br>  <button type="button" class="btn btn-success form-control" id="add_item" >Add</button></div>
-                    <div class="form-group col-md-2">  <br><button type="button" class="delete-row btn btn-danger form-control">Delete</button></div>
-                  </div>
-                      <table id="new-table" class="table table-striped table-hover">
-                           <thead>
-                             <tr>
-                               <th scope="col" style="width: 30px"></th>
-                               <th scope="col">Start</th>
-                               <th scope="col">End</th>
-                               <th scope="col">Price</th>
-                               <th scope="col" style="width: 100px" >Order</th>
-                             </tr>
-                           </thead>
-
-                           <tbody>
-                           </tbody>
-                        </table>
-                    </div>
-                    <div class=" col-md-12 form-group">
-                    </div>
-                    <div class=" col-md-3 form-group">
-                        <label for="signed" class=" col-md-12 control-label">Auto Create Inverse</label>
-                        <label class="radio-inline">
-                          <input type="radio"  name="has_inverse" value="1" checked> Yes</label>
-                        </label>
-                       <label class="radio-inline">
-                          <input type="radio"  name="has_inverse" value="0" > No</label>
-                       </label>
-                    </div>
-                    <div class=" col-md-3 form-group">
-                        <label for="signed" class=" col-md-12 control-label">Status</label>
-                        <label class="radio-inline">
-                          <input type="radio" id="Active" name="status" value="1" checked> Active</label>
-                        </label>
-                       <label class="radio-inline">
-                          <input type="radio" id="Deactive" name="status" value="0" > Deactive</label>
-                       </label>
-                    </div>
-                  </div>
-              </div>
-              <!-- /.box-body -->
-              <div class="box-footer">
-              <div class="form-group col-md-12">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </div>
-              </div>
-            </form>
-            </div>
-
-            <!-- /.row -->
-            </div>
-            <!-- ./card-body -->
-
-            <!-- /.card-footer -->
         </div>
-        <!-- /.card -->
         </div>
-        <!-- /.col -->
     </div>
 </div>
 @stop
@@ -168,41 +81,146 @@
 
 @section('js')
    @parent
-   <script>
-       $(function () {
-         $('div.alert').not('.alert-danger').delay(5000).fadeOut(350);
-         $('.select2').select2();
-         $('#add_item').on('click',function(e){
-        e.preventDefault();
-        //get selected option
-        var uniqueid = Date.now();
-        var routeid = $('#item-selector').find(":selected").data('startitemid');
-        var endid = $('#item-selector2').find(":selected").data('enditemid');
-        var startname = $('#item-selector').find(":selected").data('startitemname');
-        var endname = $('#item-selector2').find(":selected").data('enditemname');
-        var markup = "<tr item-id='"+uniqueid+"'><td><input type='checkbox' name='checkeditem[]'></td><td ><input type='hidden' value='"+routeid+"' name='stopover_startid[]'><input type='text' class='form-control' name='name1'size='4' value='"+startname+"' readonly /></td><td ><input type='hidden' value='"+endid+"' name='stopover_endid[]'><input type='text' class='form-control' name='name1'size='4' value='"+endname+"' readonly /></td><td><input type='text' class='form-control' name='stopover_price[]'size='4' value='0'  required /></td><td><input type='text' class='form-control' name='stopover_order[]'size='4' value='0'  required /></td></tr>";
+   <script type="text/javascript">
+       'use strict';
 
-          var exists =  0;
-         $("table tbody").find("tr").each(function () {
-             var current_stock_id = $(this).attr('item-id');
-             if(current_stock_id == uniqueid)
-             {
-               exists = exists + 1;
-             }
-           });
-         if(exists == 0)
-         {
-                 $("#new-table tbody").append(markup);
-         }
-       });
-      // Find and remove selected table rows
-       $(".delete-row").click(function(){
-           $("table tbody").find('input[name="checkeditem[]"]').each(function(){
-               if($(this).is(":checked")){
-                   $(this).parents("tr").remove();
-               }
-           });
-       });
-       })
-</script>
+        function generateRoutes(stations)
+        {
+            var routes = [];
+
+            var x;
+
+            for(x = 0; x<stations.length; x++) {
+                var y
+                for(y=(x+1); y<stations.length; y++) {
+                    var route = {
+                        "from": stations[x],
+                        "to": stations[y]
+                    };
+                    routes.push(route);
+                }
+            }
+
+            return routes;
+        }
+
+        $(document).ready(function() {
+            var stations = [];
+
+            $('select[name=station]').select2({
+                width: '100%',
+                allowClear: true,
+                minimumInputLength: 2,
+                placeholder: "Choose station...",
+                ajax: {
+                    type: 'GET',
+                    url: 'http://localhost:8000/transit/stations/suggest',
+                    dataType: 'json',
+                    delay: 250,
+                    cache: true,
+                    data: function (params) {
+                        return {
+                            station: params.term,
+                            limit: 10
+                        };
+                    },
+                    processResults: function (data) {
+                        return {
+                            results: data.stations.map(function (station) {
+                                return {
+                                    id: station.id,
+                                    text: station.name
+                                };
+                            })
+                        };
+                    },
+                    error: function (xhr) {
+                        console.error(xhr);
+                    }
+                }
+            });
+
+            $('#add-stations-form').on('submit', function(e) {
+                e.preventDefault();
+
+                var data = $('select[name=station]').select2('data')[0];
+
+                var stationIds = stations.map(function(station) {
+                    return station.id;
+                });
+
+                if (stationIds.indexOf(data.id) !== -1) {
+                    return;
+                }
+
+                var chosenTime = $('input[name=time]').val();
+
+                var noOfStations = stations.length;
+
+                if(stations.length) {
+                    var lastStation = stations[(noOfStations - 1)];
+
+                    console.log(lastStation.time + ' is after ' + chosenTime + '?');
+
+                    if(moment(lastStation.time, "HH:mm").isAfter(moment(chosenTime, "HH:mm"))) {
+                        alert('Last station time was '+lastStation.time);
+                        return;
+                    }
+                }
+
+                stations.push({
+                    id: data.id,
+                    name: data.text,
+                    time: chosenTime
+                });
+
+                console.log({stations: stations});
+
+                var input = '<div class="col-md-3 mb-4"><input type="text" name="stations[]" class="form-control" value="'+data.text+'" readonly/><div>';
+
+                var old_stations = $('div#stations').html();
+
+                $('div#stations').html(old_stations+input);
+
+                $('select[name=station]').val(null).trigger('change');
+
+                $('input[name=time]').val('');
+            });
+
+            $('input[id=remove]').on('click', function() {
+                $('input[name="stations[]"]:last').closest('div').remove();
+                stations.pop();
+            });
+
+            $('input[id=generate]').on('click', function() {
+                $('table[id=routes-tbl] tbody').html('');
+
+                var routes = generateRoutes(stations);
+
+                console.log({routes: routes});
+
+                routes.forEach(function(route, index) {
+                    var row = '';
+
+                    row += '<tr>';
+                    row += '<th><i class="far fa-trash-alt text-danger" id="del_route"></i></th>';
+                    row += '<td><select class="form-control" name="routes['+index+'][from]" required><option value="'+route.from.id+'">'+route.from.name+'</option></select></td>';
+                    row += '<td><select class="form-control" name="routes['+index+'][to]" required><option value="'+route.to.id+'">'+route.to.name+'</option></select></td>';
+                    row += '<td><input type="time" class="form-control" name="routes['+index+'][in]" value="'+route.from.time+'" required></td>';
+                    row += '<td><input type="time" class="form-control" name="routes['+index+'][out]" value="'+route.to.time+'" required></td>';
+                    row += '<td><input type="text" class="form-control" name="routes['+index+'][price]" value="" required></td>';
+                    row += '<td><input type="text" class="form-control" name="routes['+index+'][order]" value="'+index+'" required></td>';
+                    row += '</tr>';
+
+                    $('table[id=routes-tbl] tbody').append(row);
+                });
+            });
+
+            $('table[id=routes-tbl] tbody').on('click', '#del_route', function() {
+                if(confirm("Are you sure?")) {
+                    $(this).closest('tr').remove();
+                }
+            });
+        });
+   </script>
 @stop
