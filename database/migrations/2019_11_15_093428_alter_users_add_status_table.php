73e2b2bc8d7e9c40b519e3647126d4f6eab1a28c
Raymond Byaruhanga <rayim25@gmail.com>
2020-05-25 20:07:22 +0300

    missing migrations

---
+++ database/migrations/2019_11_15_093428_alter_users_add_status_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersAddStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public $tableName='users';
+    public $columnName='status';
+    public function up()
+    {
+        Schema::table($this->tableName, function ($table) {
+            if (!Schema::hasColumn($this->tableName, $this->columnName)) {
+                $table->tinyInteger($this->columnName)->default(0);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->dropColumn($this->columnName);
+        });
+    }
+}
---
+++ database/migrations/2020_05_18_071630_add_workstation_column_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddWorkstationColumnUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public $tableName='users';
+    public $columnName='workstation';
+    public function up()
+    {
+        Schema::table($this->tableName, function ($table) {
+            if (!Schema::hasColumn($this->tableName, $this->columnName)) {
+                $table->unsignedBigInteger($this->columnName)->nullable();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->dropColumn($this->columnName);
+        });
+    }
+}
